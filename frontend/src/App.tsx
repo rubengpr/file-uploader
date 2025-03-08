3304e0709c264ac3a6c4293cbc87f383943c36ab
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-03-08 20:46:56 +0100

    style(landing): landing v1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className="font-mono bg-black min-h-screen">
-      <nav className="w-full h-14 flex items-center p-4">
+      <nav className="navbar w-full h-14 flex items-center p-4">
         <div className="w-full h-full flex justify-start items-center">
           <img className="w-24 h-auto pr-6" src="./public/vercel-logo.svg" alt="Website logo" />
           <ul className="flex flex-row justify-center items-center text-sm text-white gap-4">
@@ -14,16 +14,25 @@ function App() {
           </ul>
         </div>
         <div className="w-full h-full flex justify-end items-center">
-          <button className="bg-black text-white px-4 py-1.5 rounded-sm text-sm cursor-pointer">Log in</button>
+          <button className="cursor-pointer hover:bg-gray-800 text-white px-4 py-1.5 rounded-sm text-sm border border-white ">Log in</button>
         </div>
       </nav>
-        <div className="w-full mt-20 min-h-screen justify-center items-center">
+      <div className="hero-page w-full mt-30 h-60 justify-center items-center">
           <div className="flex flex-col justify-center items-center">
             <h1 className="text-4xl font-bold text-white mb-6 tracking-tighter">Store your files (if you dare).</h1>
             <div className="w-150">
               <h3 className="text-md text-gray-200 text-center tracking-tighter mb-1">Store your files safely (I believe) & support a young pal.</h3>
               <h3 className="text-md text-gray-200 text-center tracking-tighter">Google is spying you.</h3>
             </div>
+            <div className="flex flex-row justify-center items-center">
+              <img src="" alt="" />
+            </div>
+          </div>
+      </div>
+      <div className="footer w-full h-14 flex flex-row justify-center items-center p-4">
+        <div className="container flex flex-row justify-center items-center gap-2">
+          <img className="w-8" src="/public/github-logo.png" alt="Github logo" />
+          <p className="text-md text-white">Made by rubengpr</p>
         </div>
       </div>
     </div>
