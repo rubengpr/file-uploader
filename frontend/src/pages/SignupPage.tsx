a05495525995b28ec055f75e121d7c9205b31aad
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-03-10 12:23:05 +0100

    style: add m, p to login & register pages

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom"
 
-export default function LoginPage() {
+export default function SignupPage() {
     return(
         <div className="login-page flex flex-row justify-center items-center font-mono text-white bg-black min-h-screen">
             <div className="login-container bg-gray-700 border-solid border-white rounded-md">
                 <div className="login-header flex flex-row justify-center items-center py-3 text-xl font-bold">
-                    <h2>Log in</h2>
+                    <h2>Sign up</h2>
                 </div>
                 <form className="px-12 py-6" action="">
                     <div className="flex flex-col justify-center items-start mb-3">
@@ -16,9 +16,13 @@ export default function LoginPage() {
                         <label className="text-xs w-full" htmlFor="">Password</label>
                         <input className="border-1 rounded-sm w-50 px-2 py-1 text-xs" type="password" />
                     </div>
+                    <div className="flex flex-col justify-center items-start mb-3">
+                        <label className="text-xs w-full" htmlFor="">Repeat password</label>
+                        <input className="border-1 rounded-sm w-50 px-2 py-1 text-xs" type="password" />
+                    </div>
                     <div className="login-footer flex flex-col justify-center items-center py-3 mt-6">
-                        <button className="cursor-pointer w-full hover:bg-gray-800 text-white px-4 py-1.5 rounded-sm text-sm border border-white mb-2">Log in</button>
-                        <p className="text-xs">or <u className="cursor-pointer"><Link to="/signup">sign up</Link></u></p>
+                        <button className="cursor-pointer w-full hover:bg-gray-800 text-white px-4 py-1.5 rounded-sm text-sm border border-white mb-2">Sign up</button>
+                        <p className="text-xs">or <u className="cursor-pointer"><Link to="/login">log in</Link></u></p>
                     </div>
                 </form>
             </div>
--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App"
 import LoginPage from "../pages/LoginPage"
+import SignupPage from "../pages/SignupPage"
 
 const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
     {
       path: "login",
       element: <LoginPage />,
+    },
+    {
+      path: "signup",
+      element: <SignupPage />,
     },
   ]);
 
