91a095c23759f8ebc5a56fa6b7b8668bfd55544d
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-03-10 18:49:47 +0100

    chore(ts): uninstall ts linter

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,6 +1,24 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 export default function SignupPage() {
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    function handleBlur(e) {
+        setPassword(e.target.value)
+        validatePassword(password)
+    }
+    
+    function validatePassword(password: string) {
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/
+        if (!passwordRegex.test(password)) {
+            setError('Password must contain at least 1 uppercase letter, 1 number, and 1 special character, and be at least 8 characters long.');
+          } else {
+            setError('');
+          }
+        }
+
     return(
         <div className="login-page flex flex-row justify-center items-center font-mono text-white bg-black min-h-screen">
             <div className="login-container bg-gray-700 border-solid border-white rounded-md">
@@ -14,7 +32,7 @@ export default function SignupPage() {
                     </div>
                     <div className="flex flex-col justify-center items-start mb-3">
                         <label className="text-xs w-full" htmlFor="">Password</label>
-                        <input className="border-1 rounded-sm w-50 px-2 py-1 text-xs" type="password" />
+                        <input className="border-1 rounded-sm w-50 px-2 py-1 text-xs" type="password" onBlur={handleBlur} />
                     </div>
                     <div className="flex flex-col justify-center items-start mb-3">
                         <label className="text-xs w-full" htmlFor="">Repeat password</label>
