1d552ab806803e84cf71aa0b47554d4437438aaa
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-03-12 23:54:13 +0100

    style(input): add border on hover

--- frontend/src/components/LabelInput.tsx
+++ frontend/src/components/LabelInput.tsx
@@ -8,7 +8,7 @@ interface LabelInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     return(
         <div className="flex flex-col justify-center w-full px-4">
                 <label className="text-white text-xs w-full mb-1 pl-0.5" htmlFor="">{label}</label>
-                <input {...props} className="w-full px-1 py-1 text-white text-xs border border-white rounded-sm caret-white focus:outline-none focus:ring focus:ring-white" type={type} />
+                <input {...props} className="w-full px-1 py-1 text-white text-xs border border-gray-400 rounded-sm caret-white focus:outline-none focus:ring focus:ring-white hover:border-white" type={type} />
                 {error && (<p className="text-red-500 text-[10px] mt-1">{error}</p>)}
         </div>
     )
