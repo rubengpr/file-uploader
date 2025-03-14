05cc5990c1c8e92e119dcdc7c015bf975671f270
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-03-14 09:19:14 +0100

    fix: add missing section header

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -61,6 +61,8 @@ export default function SignupPage() {
 
     return(
         <div className="signup-page flex flex-col justify-center items-center font-mono text-white bg-black min-h-screen">
+            <img className="w-30 mb-6" src="../public/folded-logo.svg" alt="Folded logo" />
+            <p className="mb-2">One more step to store your files</p>
             <Form title="Sign up" buttonText="Sign up" belowButton={ <> or{" "} <u className="cursor-pointer"> <Link to="/login">log in</Link> </u> </> }>
                 <LabelInput label="Email" type="text" error={emailError} value={email} onChange={(e) => setEmail(e.target.value)} onBlur={handleEmailBlur} />
                 <div className="relative w-full">
