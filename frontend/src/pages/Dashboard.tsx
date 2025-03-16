53b3b06d1d9e3c5575fee6a2584a8a379f9d82ff
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-03-16 15:43:30 +0100

    feat: add dashboard v0

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+    return(
+        <>
+            <h1>Welcome to the dashboard!</h1>
+            <p>Here some random text I don't know what to put</p>
+        </>
+    )
+}
