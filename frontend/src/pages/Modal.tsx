b024e0802f7ffad4a0de6dbd5a9a17fdc958077f
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-03-18 21:01:35 +0100

    chore: modal tweaks

--- frontend/src/pages/Modal.tsx
+++ frontend/src/pages/Modal.tsx
@@ -1,12 +1,14 @@
 interface FormProps {
+    modalTitle: string;
+    modalText: string;
     children: React.ReactNode;
   }
 
-export default function Modal({ children }: FormProps) {
+export default function Modal({ modalTitle, modalText, children }: FormProps) {
     return(
         <div className="modal-container bg-gray-700 flex flex-col justify-center items-center px-10 py-8 rounded-md">
-            <h1 className="text-3xl mb-4">This is the modal title</h1>
-            <p className="text-xs w-140 text-center">This is the modal text. It should be a very long text, so just in case.</p>
+            <h1 className="text-3xl mb-4">{modalTitle}</h1>
+            <p className="text-xs w-140 text-center">{modalText}</p>
             <div className="py-4">
                 {children}
             </div>
