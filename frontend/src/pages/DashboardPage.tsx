75ff9fdc2654f7395248d0c6bf7dad9d10f5a22e
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-03-26 17:50:22 +0100

    feat: add logout function

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -16,11 +16,17 @@ export default function Dashboard() {
             }
           }, [navigate]);
 
+          const handleLogout = () => {
+            navigate('/login');
+
+            localStorage.removeItem("token")
+          }
+
     return (
         <div className='main-page flex flex-col bg-black font-mono'>
           <div className='top-bar h-12 flex flex-row justify-between items-center px-4 border-b border-white'>
             <img className='logo w-20' src="/folded-logo.svg" alt="Folded logo" />
-            <FontAwesomeIcon className='text-white cursor-pointer' icon={faRightFromBracket} />
+            <FontAwesomeIcon className='text-white cursor-pointer' icon={faRightFromBracket} onClick={handleLogout} />
           </div>
           <div className='main-page flex flex-row bg-black'>
             <div className='sidebar flex flex-col w-40 bg-black px-2 py-4 border-r border-white gap-2'>
