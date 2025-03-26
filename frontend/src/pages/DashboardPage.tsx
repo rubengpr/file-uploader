5fe4d5a3b5a638d864cf6fc7733883fccc4c5cba
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-03-26 19:13:55 +0100

    style(table): v2

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -24,12 +24,12 @@ export default function Dashboard() {
 
     return (
         <div className='main-page flex flex-col bg-black font-mono'>
-          <div className='top-bar h-12 flex flex-row justify-between items-center px-4 border-b border-white'>
+          <div className='top-bar h-12 flex flex-row justify-between items-center px-4 border-b border-gray-700'>
             <img className='logo w-20' src="/folded-logo.svg" alt="Folded logo" />
             <FontAwesomeIcon className='text-white cursor-pointer' icon={faRightFromBracket} onClick={handleLogout} />
           </div>
           <div className='main-page flex flex-row bg-black'>
-            <div className='sidebar flex flex-col w-40 bg-black px-2 py-4 border-r border-white gap-2'>
+            <div className='sidebar flex flex-col w-50 bg-black px-2 py-4 border-r border-gray-700 gap-2'>
               <div className='sidebar-option flex flex-row justify-start items-center gap-2 cursor-pointer'>
                 <div className='w-4'>
                   <FontAwesomeIcon className='text-white' icon={faFile} />
@@ -44,38 +44,38 @@ export default function Dashboard() {
               </div>
             </div>
             <div className="page-content w-full flex justify-center h-screen py-8 px-4">
-              <div className="overflow-x-auto w-full max-w-5xl rounded-xs shadow-md">
-                <table className="min-w-full text-sm text-white border border-white rounded-xl overflow-hidden">
-                  <thead className="bg-gray-700 uppercase text-xs">
+              <div className="w-full h-fit max-w-5xl rounded-md shadow-md border border-gray-500">
+                <table className="min-w-full text-white rounded-md bg-neutral-900">
+                  <thead className="text-xs border-b border-white">
                     <tr>
-                      <th className="px-6 py-3 text-left border-b border-white">ID</th>
-                      <th className="px-6 py-3 text-left border-b border-white">Name</th>
-                      <th className="px-6 py-3 text-left border-b border-white">Email</th>
-                      <th className="px-6 py-3 text-left border-b border-white">Role</th>
-                      <th className="px-6 py-3 text-left border-b border-white">Status</th>
+                      <th className="px-6 py-2 text-left">File name</th>
+                      <th className="px-6 py-2 text-left">Created at</th>
+                      <th className="px-6 py-2 text-left">Size</th>
+                      <th className="px-6 py-2 text-left">Created by</th>
+                      <th className="px-6 py-2 text-left"></th>
                     </tr>
                   </thead>
-                  <tbody className="bg-gray-800 divide-y divide-white/10">
-                    <tr className="hover:bg-gray-700">
-                      <td className="px-6 py-2">1</td>
-                      <td className="px-6 py-2">Alice Smith</td>
-                      <td className="px-6 py-2">alice@example.com</td>
-                      <td className="px-6 py-2">Admin</td>
-                      <td className="px-6 py-2">Active</td>
+                  <tbody className="divide-y divide-white/10">
+                    <tr className="hover:bg-neutral-800 cursor-pointer text-gray-300 hover:text-white">
+                      <td className="px-6 py-2 text-xs">myscanneddocument.pdf</td>
+                      <td className="px-6 py-2 text-xs">Jan 28th 17:30h</td>
+                      <td className="px-6 py-2 text-xs">12 MB</td>
+                      <td className="px-6 py-2 text-xs">Ruben Godoy</td>
+                      <td className="px-6 py-2 text-xs"></td>
                     </tr>
-                    <tr className="hover:bg-gray-700">
-                      <td className="px-6 py-2">2</td>
-                      <td className="px-6 py-2">Bob Johnson</td>
-                      <td className="px-6 py-2">bob@example.com</td>
-                      <td className="px-6 py-2">User</td>
-                      <td className="px-6 py-2">Inactive</td>
+                    <tr className="hover:bg-neutral-800 cursor-pointer text-gray-200 hover:text-white">
+                      <td className="px-6 py-2 text-xs">myimages.jpg</td>
+                      <td className="px-6 py-2 text-xs">Jan 28th 17:30h</td>
+                      <td className="px-6 py-2 text-xs">30 MB</td>
+                      <td className="px-6 py-2 text-xs">Jessica Brownie</td>
+                      <td className="px-6 py-2 text-xs"></td>
                     </tr>
-                    <tr className="hover:bg-gray-700">
-                      <td className="px-6 py-2">3</td>
-                      <td className="px-6 py-2">Charlie Brown</td>
-                      <td className="px-6 py-2">charlie@example.com</td>
-                      <td className="px-6 py-2">Editor</td>
-                      <td className="px-6 py-2">Pending</td>
+                    <tr className="hover:bg-neutral-800 cursor-pointer text-gray-200 hover:text-white">
+                      <td className="px-6 py-2 text-xs">worddocumentwithalongnamesoitstruncated.doc</td>
+                      <td className="px-6 py-2 text-xs">Jan 28th 17:30h</td>
+                      <td className="px-6 py-2 text-xs">135 KB</td>
+                      <td className="px-6 py-2 text-xs">Rose Red</td>
+                      <td className="px-6 py-2 text-xs"></td>
                     </tr>
                   </tbody>
                 </table>
