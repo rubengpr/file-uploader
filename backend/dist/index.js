288604cad839f737ba0888b217149c924429f81b
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-04-07 23:32:54 +0200

    chore: build backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,7 +5,10 @@ import authRoutes from './routes/auth.js';
 import fileRoutes from './routes/fileRoutes.js';
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://folded.me",
+    credentials: true,
+}));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/file', fileRoutes);
