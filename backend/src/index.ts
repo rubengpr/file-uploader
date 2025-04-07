5c643845942d6ecc803b60bab1e7a69bc882916e
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-04-07 23:31:29 +0200

    fix: update cors policy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,11 @@ import fileRoutes from './routes/fileRoutes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://folded.me",
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
