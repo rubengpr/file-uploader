9c126edee050166d289ff0e0f17507cbb0cdaabd
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-05-01 15:31:27 +0200

    fix: add text-white to all modal h1, p

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -30,7 +30,7 @@ export default function Modal({ modalTitle, modalText, children, onClose }: Moda
   return (
       <div className="fixed inset-0 bg-black/50 z-40">
         <div className="fixed inset-0 z-50 flex items-center justify-center" onClick={handleBackdropClick}>
-            <div className="p-6 rounded-lg shadow-lg bg-neutral-700 flex flex-col items-center">
+            <div className="p-6 rounded-lg shadow-lg bg-neutral-700 flex flex-col items-center text-white">
                 <h1 className="text-3xl mb-4 font-bold">{modalTitle}</h1>
                 <p className="text-xs w-140 text-center">{modalText}</p>
                 <div className="py-4 w-1/2">
