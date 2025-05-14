a9aa5f94fa3e76e6d1c6f6100a568445a6cc4a9f
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-05-14 18:25:14 +0200

    chore: add inputSize prop on Input

--- frontend/src/pages/FoldersPage.tsx
+++ frontend/src/pages/FoldersPage.tsx
@@ -1,8 +1,7 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { isAuthenticated, isTokenExpired } from '../utils/auth.ts';
 import { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightFromBracket, faFilter } from '@fortawesome/free-solid-svg-icons'
+import { faFilter } from '@fortawesome/free-solid-svg-icons'
 import Table from '../components/Table.tsx';
 import Sidebar from '../components/Sidebar.tsx';
 import Input from '../components/Input.tsx';
@@ -13,6 +12,7 @@ import Modal from '../components/Modal.tsx';
 import Button from '../components/Button.tsx';
 import Selector from '../components/Selector.tsx';
 import DatePicker from '../components/DatePicker.tsx';
+import MainLayout from './MainLayout.tsx';
 
 export default function FoldersPage() {
 
@@ -28,7 +28,7 @@ export default function FoldersPage() {
   const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
   const [date, setDate] = useState("");
   const [type, setType] = useState("All types");
-
+  
   interface File {
     id: string;
     name: string;
@@ -41,158 +41,144 @@ export default function FoldersPage() {
     }
   };
 
-          interface Folder {
-            id: string;
-            name: string;
-            createdAt: string;
-            createdBy: string;
-            user: {
-              email: string;
-            };
-          }
-
-          const handleLogout = () => {
-            navigate('/login');
-
-            localStorage.removeItem("token")
-          }
+  interface Folder {
+    id: string;
+    name: string;
+    createdAt: string;
+    createdBy: string;
+    user: {
+      email: string;
+    };
+  }
         
-          useEffect(() => {
-            if (!isAuthenticated()) {
-              navigate('/login');
-            }
-          }, [navigate]);
+  useEffect(() => {
+    if (!isAuthenticated()) {
+      navigate('/login');
+    }
+  }, [navigate]);
 
-          useEffect(() => {
-            const refreshToken = async () => {
-              if (isTokenExpired()) {
-                try {
-                  const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/refresh`, {}, { withCredentials: true });
-                  const { token } = response.data;
-                  localStorage.setItem('token', token);
-                } catch(error) {
-                if (axios.isAxiosError(error)) {
-                    const message = error.response?.data?.error || "Error refreshing the token";
-                    showErrorToast(message);
-                    handleLogout();
-                } else {
-                    showErrorToast("Unexpected error occurred.");
-                    handleLogout();
-                }
-              }}
-            }
-            
-            refreshToken()
-          }, [navigate, handleLogout]);
-          
-
-          const getFiles = async (folderId: string) => {
-            
-            try {
-                const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/file/get/${folderId}`);
-                setFiles(response.data);
-                setAllFiles(response.data);
-            } catch(error) {
-                if (axios.isAxiosError(error)) {
-                    const message = error.response?.data?.error || "Error getting the files.";
-                    showErrorToast(message);
-                } else {
-                    showErrorToast("Unexpected error occurred.");
-                }
-            }
-          }
-
-          const getFolders = async (folderId: string) => {
-            try {
-                const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/folder/get/${folderId}`);
-                setFolders(response.data);
-            } catch(error) {
-                if (axios.isAxiosError(error)) {
-                    const message = error.response?.data?.error || "Error getting the folders";
-                    showErrorToast(message);
-                } else {
-                    showErrorToast("Unexpected error occurred.");
-                }
-            }
-          }
-
-          const updateTable = () => {
-            const id = folderId ?? "root";
-            getFiles(id);
-            getFolders(id);
-            handleSort("createdAt");
-          }
+  useEffect(() => {
+    const refreshToken = async () => {
+      if (isTokenExpired()) {
+        try {
+          const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/refresh`, {}, { withCredentials: true });
+          const { token } = response.data;
+          localStorage.setItem('token', token);
+        } catch(error) {
+        if (axios.isAxiosError(error)) {
+            const message = error.response?.data?.error || "Error refreshing the token";
+            showErrorToast(message);
+        } else {
+            showErrorToast("Unexpected error occurred.");
+        }
+      }}
+    }
     
-          useEffect(() => {
-            const id = folderId ?? "root";
-            getFiles(id);
-            getFolders(id);
-          }, [folderId]);
+    refreshToken()
+  }, [navigate]);
 
-          const handleSort = (key: keyof File) => {
-            if (sortKey === key) {
-              setSortDirection(sortDirection === "asc" ? "desc" : "asc");
-            } else {
-              setSortKey(key);
-              setSortDirection("asc");
-            }
-          
-            const sorted = [...files].sort((a, b) => {
-              const aVal = a[key];
-              const bVal = b[key];
-          
-              if (typeof aVal === "number" && typeof bVal === "number") {
-                return sortDirection === "asc" ? aVal - bVal : bVal - aVal;
-              }
-          
-              if (key === "createdAt") {
-                const aDate = new Date(aVal as string).getTime();
-                const bDate = new Date(bVal as string).getTime();
-                return sortDirection === "asc" ? aDate - bDate : bDate - aDate;
-              }
-          
-              return sortDirection === "asc"
-                ? String(aVal).localeCompare(String(bVal))
-                : String(bVal).localeCompare(String(aVal));
-            });
-          
-            setFiles(sorted);
-          };
+  const getFiles = async (folderId: string) => {
+    
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/file/get/${folderId}`);
+        setFiles(response.data);
+        setAllFiles(response.data);
+    } catch(error) {
+        if (axios.isAxiosError(error)) {
+            const message = error.response?.data?.error || "Error getting the files.";
+            showErrorToast(message);
+        } else {
+            showErrorToast("Unexpected error occurred.");
+        }
+    }
+  }
 
-          const handleSearcherChange = (value: string) => {
-            setSearcherValue(value);
-            const filteredArray = searchFiles(value);
-            setFiles(filteredArray);
-          };          
+  const getFolders = async (folderId: string) => {
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/folder/get/${folderId}`);
+        setFolders(response.data);
+    } catch(error) {
+        if (axios.isAxiosError(error)) {
+            const message = error.response?.data?.error || "Error getting the folders";
+            showErrorToast(message);
+        } else {
+            showErrorToast("Unexpected error occurred.");
+        }
+    }
+  }
 
-          const searchFiles = (searchText: string): File[] => {
-            if (!searchText.trim()) return allFiles;
-          
-            return allFiles.filter((file) =>
-              file.name.toLowerCase().includes(searchText.toLowerCase())
-            );
-          };
+  const updateTable = () => {
+    const id = folderId ?? "root";
+    getFiles(id);
+    getFolders(id);
+    handleSort("createdAt");
+  }
+    
+  useEffect(() => {
+    const id = folderId ?? "root";
+    getFiles(id);
+    getFolders(id);
+  }, [folderId]);
 
-          const handleFilter = () => {
-            const filtered = filterFiles(allFiles, type, date)
-            setFiles(filtered);
-            setIsFilterModalOpen(false);
-          }
-          
-          const filterFiles = (files: File[], type: string, date: string): File[] => {
-            return files.filter(file =>
-              (type === "All types" || file.type === type) &&
-              new Date(file.createdAt) >= new Date(date)
-            );
-          };          
-          
+  const handleSort = (key: keyof File) => {
+    if (sortKey === key) {
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    } else {
+      setSortKey(key);
+      setSortDirection("asc");
+    }
+  
+    const sorted = [...files].sort((a, b) => {
+      const aVal = a[key];
+      const bVal = b[key];
+  
+      if (typeof aVal === "number" && typeof bVal === "number") {
+        return sortDirection === "asc" ? aVal - bVal : bVal - aVal;
+      }
+  
+      if (key === "createdAt") {
+        const aDate = new Date(aVal as string).getTime();
+        const bDate = new Date(bVal as string).getTime();
+        return sortDirection === "asc" ? aDate - bDate : bDate - aDate;
+      }
+  
+      return sortDirection === "asc"
+        ? String(aVal).localeCompare(String(bVal))
+        : String(bVal).localeCompare(String(aVal));
+    });
+  
+    setFiles(sorted);
+  };
+
+  const handleSearcherChange = (value: string) => {
+    setSearcherValue(value);
+    const filteredArray = searchFiles(value);
+    setFiles(filteredArray);
+  };          
+
+  const searchFiles = (searchText: string): File[] => {
+    if (!searchText.trim()) return allFiles;
+  
+    return allFiles.filter((file) =>
+      file.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+  };
+
+  const handleFilter = () => {
+    const filtered = filterFiles(allFiles, type, date)
+    setFiles(filtered);
+    setIsFilterModalOpen(false);
+  }
+  
+  const filterFiles = (files: File[], type: string, date: string): File[] => {
+    return files.filter(file =>
+      (type === "All types" || file.type === type) &&
+      new Date(file.createdAt) >= new Date(date)
+    );
+  };     
         
     return (
-        <div className='main-page flex flex-col bg-black'>
-          <div className='top-bar h-12 flex flex-row justify-between items-center px-4 border-b border-gray-700'>
-            <img className='logo w-20' src="/folded-logo.svg" alt="Folded logo" />
-            <FontAwesomeIcon className='text-white cursor-pointer' icon={faRightFromBracket} onClick={handleLogout} />
-          </div>
+        <MainLayout>
           <div className='main-page flex flex-row bg-black'>
             <Sidebar onUploadSuccess={updateTable} />
             <div className="page-content w-full flex flex-col h-screen px-10 py-8">
@@ -204,6 +190,7 @@ export default function FoldersPage() {
                 <Input
                   type="text"
                   placeholder='Search...'
+                  inputSize='sm'
                   value={searcherValue}
                   handleInputChange={handleSearcherChange} />
               </div>
@@ -307,6 +294,6 @@ export default function FoldersPage() {
                 </div>
               </Modal>
           )}
-        </div>
+        </MainLayout>
     );
 };
