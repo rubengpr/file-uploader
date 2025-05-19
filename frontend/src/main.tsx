adddf8d1d550070b678f3322feed16ca08b42812
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-05-19 14:34:19 +0200

    chore: install posthog

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,18 @@ import { RouterProvider } from "react-router-dom";
 import './styles/globals.css'
 import router from "./routes/routes.tsx"
 
+import { PostHogProvider } from 'posthog-js/react'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        debug: import.meta.env.MODE === 'development',
+      }}
+    >
+      <RouterProvider router={router} />
+    </PostHogProvider>
   </StrictMode>,
 )
