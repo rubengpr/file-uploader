b83a3e9994a382cf8e471aed66270b4c403e67d7
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-05-24 10:39:24 +0200

    chore: alphabetize main.tsx imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
-import './styles/globals.css'
-import router from "./routes/routes.tsx"
-
+import { StrictMode } from 'react'
 import { PostHogProvider } from 'posthog-js/react'
+import router from "./routes/routes.tsx"
+import './styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
