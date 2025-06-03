a1e434695a2df84d80b0722371b1217793421762
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-06-03 08:54:52 +0200

    feat: add navigation on logo click

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,12 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 interface TopbarProps {
     avatar: string;
     handleAvatarClick: () => void;
 }
 
 export default function Topbar({ avatar, handleAvatarClick }: TopbarProps) {
+    const navigate = useNavigate()
+
     return(
         <div className='top-bar h-12 flex flex-row justify-between items-center px-5 border-b border-gray-700'>
-            <img className='logo w-20' src="/folded-logo.svg" alt="Folded logo" />
+            <img onClick={() => navigate('/folders')} className='logo w-20 cursor-pointer' src="/folded-logo.svg" alt="Folded logo" />
             <img onClick={handleAvatarClick} className='w-7 h-7 rounded-full scale-100 cursor-pointer' src={avatar} alt="User avatar" />
         </div>
     )
