a7501d5816a61dd264ca244fb63d23e74d70e3df
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-07-05 14:18:56 +0200

    feat: add JSON payload size limit to 10kb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -46,6 +46,8 @@ app.use(cookieParser())
 app.use(cors(corsOptions))
 app.options('*', cors(corsOptions))
 app.use(express.json())
+app.use(express.json({ limit: '10kb' })); // Limit JSON payload size
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 app.use((req, res, next) => {
   next();
