b8b4cdbd9d60b96cc24dd16346c2f4ed9f355a5b
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2025-08-02 14:27:57 +0200

    feat: add ui plans logic for current plans
    * chore: update User store when accessing /settings
    
    * feat: add Tag component
    
    * misc: configure Tailwind Intellisense
    
    * feat: add conditional rendering of Tag for current plan
    
    * feat: add disabled logic for plan purchasing

--- frontend/src/pages/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage.tsx
@@ -2,6 +2,8 @@ import MainLayout from "./MainLayout"
 import axios from "axios";
 import { showErrorToast } from "@/utils/toast";
 import useUser from "@/stores/useUser";
+import { useEffect } from "react";
+import Tag from "@/components/Tag";
 
 const handleSubscription = async (planType, userId) => {
 
@@ -30,7 +32,32 @@ const handleSubscription = async (planType, userId) => {
 }
 
 export default function SettingsPage() {
-    const { userId } = useUser()
+    const { userId, currentPlan } = useUser()
+
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                const token = localStorage.getItem('token')
+                const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/profile/me`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+                const user = response.data.user
+                useUser.getState().setUser(user)
+
+            } catch(error) {
+                if (axios.isAxiosError(error)) {
+                    const message = error.response?.data?.error
+                    showErrorToast(message)
+                } else {
+                    showErrorToast("Unexpected error occurred")
+                }
+            }
+        }
+
+        getUser()
+    }, [])
 
     return(
         <MainLayout>
@@ -46,21 +73,36 @@ export default function SettingsPage() {
                                 <p className="text-white text-lg">Free</p>
                                 <span className="text-neutral-400 text-sm">5 files, 1 user and unlimited folders</span>
                             </div>
-                            <button className="text-white text-sm border px-4 py-1 bg-neutral-900 rounded-full hover:cursor-pointer hover:bg-neutral-800">Get Free</button>
+                            <div className="flex flex-row justify-center items-center gap-4">
+                                {currentPlan === 'free' && (
+                                    <Tag />
+                                )}
+                                <button disabled={currentPlan === 'free'} className="text-white text-sm border px-4 py-1 bg-neutral-900 rounded-full hover:cursor-pointer hover:bg-neutral-800 disabled:bg-neutral-700 disabled:text-neutral-400 disabled:hover:cursor-not-allowed">Get Free</button>
+                            </div>
                         </div>
                         <div className="flex flex-row justify-between items-center border-b border-neutral-700 px-2 py-4">
                             <div className="flex flex-col">
                                 <p className="text-white text-lg">Standard</p>
                                 <span className="text-neutral-400 text-sm">20 files, 3 users and unlimited folders</span>
                             </div>
-                            <button onClick={() => handleSubscription('standard', userId)} className="text-white text-sm border px-4 py-1 bg-neutral-900 rounded-full hover:cursor-pointer hover:bg-neutral-800">Get Standard</button>
+                            <div className="flex flex-row justify-center items-center gap-4">
+                                {currentPlan === 'standard' && (
+                                    <Tag />
+                                )}
+                                <button disabled={currentPlan === 'standard'} onClick={() => handleSubscription('standard', userId)} className="text-white text-sm border px-4 py-1 bg-neutral-900 rounded-full hover:cursor-pointer hover:bg-neutral-800 disabled:bg-neutral-700 disabled:text-neutral-400 disabled:hover:cursor-not-allowed">Get Standard</button>
+                            </div>
                         </div>
                         <div className="flex flex-row justify-between items-center px-2 py-4">
                             <div className="flex flex-col">
                                 <p className="text-white text-lg">Max</p>
                                 <span className="text-neutral-400 text-sm">Unlimited everything</span>
                             </div>
-                            <button onClick={() => handleSubscription('max', userId)} className="text-white text-sm border px-4 py-1 bg-neutral-900 rounded-full hover:cursor-pointer hover:bg-neutral-800">Get Max</button>
+                            <div className="flex flex-row justify-center items-center gap-4">
+                                {currentPlan === 'max' && (
+                                    <Tag />
+                                )}
+                                <button disabled={currentPlan === 'max'} onClick={() => handleSubscription('max', userId)} className="text-white text-sm border px-4 py-1 bg-neutral-900 rounded-full hover:cursor-pointer hover:bg-neutral-800 disabled:bg-neutral-700 disabled:text-neutral-400 disabled:hover:cursor-not-allowed">Get Max</button>
+                            </div>
                         </div>
                     </div>
                 </div>
