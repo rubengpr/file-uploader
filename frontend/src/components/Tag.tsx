201163a709c8114439692b2bedf6861a6b5a869a
rubengpr <61808374+rubengpr@users.noreply.github.com>
2025-08-02 13:59:38 +0200

    feat: add Tag component

---
+++ frontend/src/components/Tag.tsx
@@ -0,0 +1,7 @@
+export default function Tag() {
+    return(
+        <div className="flex flex-row justify-center items-center border border-neutral-300 px-2 py-0.5 bg-neutral-800 rounded-md">
+            <p className="text-neutral-300 font-bold text-xs">Current plan</p>
+        </div>
+    )
+}
