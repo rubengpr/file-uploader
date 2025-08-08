00b20613af1dcbe5db1ba8f9fcccfebc4b67faf5
rubengpr <61808374+rubengpr@users.noreply.github.com>
2025-08-08 18:57:52 +0200

    test: add pricing page tests

---
+++ frontend/tests/e2e/landing/landing-page.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Landing Page', () => {
+    test('should navigate to pricing page', async ({ page }) => {
+        await page.goto('/')
+        
+        await page.getByRole('link', { name: 'Pricing' }).click()
+
+        await expect(page.getByRole('heading', { name: 'Storage plans for all use cases' })).toBeVisible()
+    })
+
+    test('should show correct pricing amounts', async ({ page }) => {
+        await page.goto('/')
+
+        await page.getByRole('link', { name: 'Pricing' }).click()
+
+        await expect(page.getByRole('heading', { name: 'Hobby' })).toBeVisible()
+        await expect(page.getByRole('heading', { name: 'Standard' })).toBeVisible()
+        await expect(page.getByRole('heading', { name: 'Max' })).toBeVisible()
+    })
+
+    test('should show correct pricing names', async ({ page }) => {
+        await page.goto('/')
+
+        await page.getByRole('link', { name: 'Pricing' }).click()
+
+        await expect(page.getByRole('heading'))
+    })
+})
